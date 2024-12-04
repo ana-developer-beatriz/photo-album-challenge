231b297182290feaf4ef8847ae2c0fc666985729
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2024-12-04 12:38:41 -0300

    [feat]update files organization

--- photo-album-challenge/src/backend/src/models/IPhoto.ts
+++
@@ -1,8 +0,0 @@
-export interface IPhoto {
-  id: string;
-  albumId: number;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
-  user_id: string;
-}
--- photo-album-challenge/src/backend/src/models/IUser.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  _id: string;
-  name: string;
-  email: string;
-  role: string;
-  active: boolean;
-  password_hash: string;
-  session_token: string;
-}
