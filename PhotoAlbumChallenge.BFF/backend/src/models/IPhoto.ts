fd13efd0df559841b2fae3e3c0f786ab0bbfaa9e
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2024-12-04 12:46:55 -0300

    Project Interfaces

---
+++ PhotoAlbumChallenge.BFF/backend/src/models/IPhoto.ts
@@ -0,0 +1,8 @@
+export interface IPhoto {
+  id: string;
+  albumId: string;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+  user_id: string;
+}
---
+++ PhotoAlbumChallenge.BFF/backend/src/models/IUser.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  active: boolean;
+  password_hash: string;
+  session_token: string;
+}
